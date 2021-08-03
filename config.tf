416a2d6f3c9c4d37e807c59e2fccf7b3e802b001
kalhaarsavaj <kalhaarsavaj7@gmail.com>
2021-08-03 05:59:08 -0400

    Making more number of containers

--- config.tf
+++ config.tf
@@ -19,11 +19,12 @@ resource "docker_image" "nginx" {
 
 
 resource "docker_container" "nginx-server" {
-  name = "nginx-server"
+  count = 2
+  name = "nginx-server-${count.index+1}"
   image = docker_image.nginx.latest
   ports {
     internal = 80
-    external = "8080"
+    external = "808${count.index+1}"
   }
   volumes {
     container_path  = "/usr/share/nginx/html"
